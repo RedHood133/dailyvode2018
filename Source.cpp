9f9fd0b05be276a471f440aba34c8d3c147f5284
isaac <35504057+RedHood133@users.noreply.github.com>
2018-02-12 13:45:51 -0700

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	double i = 1;
+
+
+	while (i > 0) {
+		cout << " pay your bill" << endl;
+		cin >> i;
+		cout << i*0.15 << endl;
+		system("pause");
+	}
+}
+
+	
