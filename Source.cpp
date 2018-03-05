fdeb2b7a6689ef238ae0beafeb3cf0e2d26c2f1d
isaac <35504057+RedHood133@users.noreply.github.com>
2018-03-05 13:27:38 -0700

    Add files via upload

--- Source.cpp
+++ Source.cpp
@@ -1,21 +1,38 @@
-#include<iostream>
-#include<time.h>
-#include<ctime>
+#include <iostream>
+#include<Windows.h>
 using namespace std;
+
+//function declaration
+int pointless(int m, int n);
 
 
 int main() {
-	srand(time(NULL));
+	Beep(400, 500);//other funciton call!
+	//declare two variables
+	int x;
+	int y;
+	cout << "give me two numbers" << endl;
+	cin >> x;
+	cin >> y;
 
-	
+	//function call
+	cout<<pointless(x, y);
+	system("pause");
 
-	while (1) {
-		cout << "*";
-		int snow = rand() % 100+ 1;
-	for (int i=0; i<snow; i++)
-		cout << " ";
+}//end of main
 
+
+
+//function definition
+int pointless(int m, int n) {
+	//print out a box
+	for (int i = 0; i < m; i++) {
+		for (int j = 0; j < n; j++)
+			cout << "*";
+		cout << endl;
 	}
+
+	Beep(n*100, n*100);
+
+	return m*n;
 }
-
-
