03133b54a29137ff188479b2c73839c67d8d8364
isaac <35504057+RedHood133@users.noreply.github.com>
2018-03-15 13:55:41 -0600

    Add files via upload

--- Source.cpp
+++ Source.cpp
@@ -1,23 +1,28 @@
-#include<iostream>
-#include<string>
+#include <iostream>
+#include <ctime>
+#include <string>
 using namespace std;
+
 int main() {
-	int inventory[7];
-	bool hasfive = false;
+	string input;
+	string food[5];
+	bool hasBepsi = false;
 
-	cout << "enter item" << endl << endl;
-	for (int i = 0; i < 7; i++) {
+	cout << "What is your favorite daank?" << endl;
 
-		cin >> inventory[i];
+	for (int i = 0; i < 5; i++) {
+		cin >> input;
+		food[i] = input;
 	}
-
-	cout << "your inventory item was" << endl;
-	for (int i = 0; i < 7; i++)
-		if (inventory[i] == 5)
-			hasfive = true;
-	if (hasfive = true)
-		cout << "secret number!" << endl;
+	for (int i = 0; i < 5; i++) {
+		if (food[i] == "Bepsi" || food[i] == "Bepsi")
+			hasBepsi = true;
+	}
+	if (hasBepsi == true)
+		cout << "AAAHH...bepsi" << endl;
 	else
-		cout << "you suck" << endl;
+		cout << "do you even drink bro!" << endl;
+	system("pause");
 
 }
+
