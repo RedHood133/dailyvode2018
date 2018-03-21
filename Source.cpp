98eded169244f5b5b67eb5afa0282c49c11fa198
isaac <35504057+RedHood133@users.noreply.github.com>
2018-03-21 12:52:13 -0600

    Add files via upload

--- Source.cpp
+++ Source.cpp
@@ -1,10 +1,24 @@
-#include<iostream>
-#include<windows.h>
-using namespace std;
-int main(){
-	while(1)
-	MessageBox(NULL,"You have been selected to bet a sacrafice to the dark lord Chin-Chin", "you done goof", MB_YESNO);
+#include <iostream>
+#include <string>
+#include <ctime>
 
-	
-	
+using namespace std;
+
+char input = 'y';
+
+string adj[] = { "The dark lord ", "clapper ", "Lil ", "Big ", "FLUFFY ", "Dj ", "spoopy ", "Icy ", "Young ", "Juicy ", "Retro ", "USSR ", "doki-doki ", "Orange ", "crit " };
+string noun[] = { "water", "Blitz", "smoke", "pringles", "NANO", "Noctis", "Chin-chin", "can","Dog", "leverage","tasty" };
+
+int main() {
+	srand(time(NULL));
+	cout << "Do you want to know your Rap name?" << endl;
+	while (input == 'y') {
+		cin >> input;
+
+		int ad = rand() % 15;
+		int nouns = rand() % 10;
+		cout << adj[ad] << endl;
+		cout << noun[nouns] << endl;
+
+	}
 }
